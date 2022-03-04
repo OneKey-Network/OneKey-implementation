c7191e87063b8975c4d08e9ed78b641608b61d20
Olivier Chirouze <o.chirouze@criteo.com>
2022-03-04 10:32:30 +0100

    [demo] replace webpack with rollup and clean build
    - The old build used to generate `js` and `d.ts` files everywhere
    - Also now nodemon rebuilds js files on the fly

---
+++ paf-mvp-demo-express/rollup.config.js
@@ -0,0 +1,28 @@
+import {join} from 'path';
+import {defineConfig} from 'rollup';
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from "@rollup/plugin-node-resolve";
+
+const relative = path => join(__dirname, path);
+// https://rollupjs.org/guide/en/#configuration-files
+export default [
+  defineConfig({
+    input: relative('src/cmp/js/cmp.ts'),
+    output: {
+      file: relative(`public/assets/cmp/cmp.js`),
+      format: 'umd',
+      name: 'CMP',
+      sourcemap: true
+    },
+    treeshake: 'smallest', // remove unused code
+    plugins: [
+      typescript({
+        tsconfig: relative('src/cmp/js/tsconfig.json')
+      }),
+      commonjs(),
+      nodeResolve(),
+      // terser(), // minify js output
+    ]
+  })
+];
