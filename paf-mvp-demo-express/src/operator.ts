e1d41ddbd5f69349b020bfece23508131b3648e2
Olivier Chirouze <o.chirouze@criteo.com>
2022-03-15 17:30:35 +0100

    Add authentication to operator API

--- paf-mvp-demo-express/src/operator.ts
+++ paf-mvp-demo-express/src/operator.ts
@@ -1,9 +1,21 @@
 import express from "express";
-import {operator} from "./config";
+import {advertiser, cmp, operator, portal, publisher} from "./config";
 import {addOperatorApi} from "@operator/operator-api";
 import {s2sOptions} from "./server-config";
 
 export const operatorApp = express();
 
 // This host supports the Operator API
-addOperatorApi(operatorApp, operator.host, operator.privateKey, operator.name, [operator.currentPublicKey], s2sOptions)
+addOperatorApi(
+    operatorApp,
+    operator.host,
+    operator.privateKey,
+    operator.name,
+    [operator.currentPublicKey],
+    [
+        cmp.host,
+        advertiser.host,
+        portal.host
+    ],
+    s2sOptions
+)
