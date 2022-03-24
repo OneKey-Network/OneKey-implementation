4643909cc7546463ba258473eed9cde57507b530
d.kovtun <d.kovtun@criteo.com>
2022-03-24 15:48:55 +0200

    [UI] Adjust styles for the main widget

--- paf-mvp-frontend/src/components/modal/Modal.tsx
+++ paf-mvp-frontend/src/components/modal/Modal.tsx
@@ -1,26 +1,32 @@
 import { h } from 'preact';
 import style from './style.scss';
+import grid from '../../styles/grid.scss';
 import layout from '../../styles/layouts.scss';
-import typography from '../../styles/typography.scss';
+import { Button } from '../button/Button';
+import { Cross } from '../svg/cross/Cross';
 
 interface ModalProps {
   maxWidth?: number;
+  closeBtnText?: string;
   onClose: () => void;
   children?: JSX.Element | Array<JSX.Element>;
 }
 
-export const Modal = (props: ModalProps) => {
+export const Modal = ({ children, maxWidth, onClose, closeBtnText = '' }: ModalProps) => {
   const defaultMaxWidth = 400;
   return (
     <div class={style.modalContainer}>
       <div class={style.backdrop} />
-      <div class={style.modal} style={{ maxWidth: props.maxWidth || defaultMaxWidth }}>
+      <div class={style.modal} style={{ maxWidth: maxWidth || defaultMaxWidth }}>
         <div class={layout.justifyEnd}>
-          <button class={style.closeBtn} onClick={() => props.onClose()}>
-            <span class={`${style.btnText} ${typography.textBold}`}>Close dialog</span>&times;
-          </button>
+          <Button small accent action={() => onClose()}>
+            <div class={layout.alignCenter}>
+              <b class={grid['mr-2']}>{closeBtnText}</b>
+              <Cross />
+            </div>
+          </Button>
         </div>
-        {props.children}
+        {children}
       </div>
     </div>
   );
--- paf-mvp-frontend/src/components/sub-panel/SubPanel.tsx
+++ paf-mvp-frontend/src/components/sub-panel/SubPanel.tsx
@@ -4,23 +4,26 @@ import { Button } from '../button/Button';
 
 import classes from './style.scss';
 import layout from '../../styles/layouts.scss';
+import { Cross } from '../svg/cross/Cross';
 
 interface ISubPanelProps {
   isOpen?: boolean;
   onClose?: () => void;
+  header?: string | JSX.Element | Array<JSX.Element>;
   children: string | JSX.Element | Array<JSX.Element>;
 }
 
-export const SubPanel = ({ children, onClose, isOpen }: ISubPanelProps) => {
+export const SubPanel = ({ children, onClose, isOpen, header }: ISubPanelProps) => {
   return (
     <Fragment>
       {isOpen && <div class={classes.backdrop} onClick={() => onClose()} />}
       <div class={`${classes.container} ${isOpen ? classes.open : ''}`}>
         <div class={layout.justifyEnd}>
-          <Button classList={classes.closeBtn} action={() => onClose()}>
-            &#x203A;
+          <Button accent action={() => onClose()}>
+            <Cross />
           </Button>
         </div>
+        {header}
         <div class={classes.scroll}>{children}</div>
       </div>
     </Fragment>
