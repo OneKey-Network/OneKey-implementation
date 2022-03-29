eeec4ee3378fefe1187527bac937796f95da09b3
Olivier Chirouze <o.chirouze@criteo.com>
2022-03-29 17:25:36 +0200

    [test] extract common JEST config to the root level
    To prepare unit tests in operator and demo projects

--- paf-mvp-frontend/jest.config.js
+++ paf-mvp-frontend/jest.config.js
@@ -1,19 +1,9 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('../tsconfig');
+const rootConfig = require('../jest.config');
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  ...rootConfig,
   testEnvironment: 'jsdom',
-  rootDir: '.',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/../' }),
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
-    },
-  },
-  "automock": false,
-  "resetMocks": false,
   setupFiles: [
     '<rootDir>/tests/jest-setup.js'
   ]
