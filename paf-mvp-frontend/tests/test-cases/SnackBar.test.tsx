bd99430bbbc62a08a5ae4a6a5c6f18cbb049cc13
d.kovtun <d.kovtun@criteo.com>
2022-04-06 13:29:59 +0300

    feat(unit-tests): Add tests for SnackBar component

---
+++ paf-mvp-frontend/tests/test-cases/SnackBar.test.tsx
@@ -0,0 +1,34 @@
+import { h } from 'preact';
+import '@testing-library/jest-dom';
+import { render, fireEvent, RenderResult, cleanup } from '@testing-library/preact';
+import { SnackBar } from '../../src/components/snack-bar/SnackBar';
+
+describe('SnackBar component', () => {
+  let renderResult: RenderResult;
+  const onCloseMock = jest.fn();
+  const testIcon = <i data-testid="test-icon">o</i>;
+
+  afterEach(() => {
+    cleanup();
+    onCloseMock.mockClear();
+  });
+
+  beforeEach(() => {
+    renderResult = render(<SnackBar icon={testIcon} title="title" message="Message" onClose={onCloseMock} />);
+  });
+
+  test('should render content', () => {
+    expect(renderResult.getByTestId('notification-container')).toHaveClass('open');
+    expect(renderResult.getByTestId('test-icon')).toBeInTheDocument();
+    expect(renderResult.getByText('title')).toBeInTheDocument();
+    expect(renderResult.getByText(/message/i)).toBeInTheDocument();
+  });
+
+  test('should emit close event', () => {
+    jest.useFakeTimers();
+    fireEvent.click(renderResult.getByTestId('notification-close-btn'));
+    jest.runAllTimers();
+    expect(onCloseMock.mock.calls.length).toBe(1);
+    expect(renderResult.getByText(/message/i)).not.toHaveClass('open');
+  });
+});
