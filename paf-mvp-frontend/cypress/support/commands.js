1a40ccc735459b34e21ddd6c3a793c4545995f65
Olivier Chirouze <o.chirouze@criteo.com>
2022-05-02 16:01:04 +0200

    Fix incorrect renaming in Cypress tests

--- paf-mvp-frontend/cypress/support/commands.js
+++ paf-mvp-frontend/cypress/support/commands.js
@@ -1,7 +1,7 @@
 import '@testing-library/cypress/add-commands';
 
 // ***********************************************
-// This examples commands.js shows you how to
+// This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
