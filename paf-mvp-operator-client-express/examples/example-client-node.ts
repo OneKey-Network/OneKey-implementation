8f8ada864a93053dc40def72241fe7063d7c7cce
Olivier Chirouze <o.chirouze@criteo.com>
2022-05-02 18:00:26 +0200

    Add more comments to example files

--- paf-mvp-operator-client-express/examples/example-client-node.ts
+++ paf-mvp-operator-client-express/examples/example-client-node.ts
@@ -5,8 +5,11 @@ import { getTimeStampInSec } from '@core/timestamp';
 // This is just an example of a basic client node configuration
 addClientNodeEndpoints(
   express(),
+  // Identity information: mandatory for any PAF interaction
   {
+    // Name of the PAF participant
     name: 'Example Website',
+    // Current public key
     currentPublicKey: {
       // Timestamps are expressed in seconds
       startTimestampInSec: getTimeStampInSec(new Date('2022-01-01T12:00:00.000Z')),
@@ -16,17 +19,21 @@ MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEl0278pcupaxUfiqHJ9AG9gVMyIO+
 n07PJaNI22v+s7hR1Hkb71De6Ot5Z4JLoZ7aj1xYhFcQJsYkFlXxcBWfRQ==
 -----END PUBLIC KEY-----`,
     },
+    // Email address of DPO
     dpoEmailAddress: 'dpo@examples-website.com',
+    // URL of a privacy page
     privacyPolicyUrl: new URL('https://www.example-website/privacy'),
   },
   {
     // The PAF node host name to receive requests
     hostName: 'paf.example-website.com',
+    // Current private key
     privateKey: `-----BEGIN PRIVATE KEY-----
 MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg0X8r0PYAm3mq206o
 CdMHwZ948ONyVJToeFbLqBDKi7OhRANCAASXTbvyly6lrFR+Kocn0Ab2BUzIg76f
 Ts8lo0jba/6zuFHUeRvvUN7o63lngkuhntqPXFiEVxAmxiQWVfFwFZ9F
 -----END PRIVATE KEY-----`,
   },
+  // The PAF operator host
   'example.onekey.network'
 );
