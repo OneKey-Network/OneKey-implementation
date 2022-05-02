abf4885e921bce88818e52fd500f83271c8af05f
Olivier Chirouze <o.chirouze@criteo.com>
2022-05-02 13:41:30 +0200

    Rename operator client into PAF node

--- paf-mvp-demo-express/src/pif-client-node.ts
+++ paf-mvp-demo-express/src/pif-client-node.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import { crtoOneOperatorConfig, pifCmpConfig, pifDemoPublisherConfig, PrivateConfig } from './config';
-import { addClientNodeEndpoints } from '@operator-client/client-node';
 import { addIdentityEndpoint } from '@core/express/identity-endpoint';
 import { s2sOptions } from './server-config';
 import { getTimeStampInSec } from '@core/timestamp';
-import { getHttpsOriginFromHostName } from '@core/express/utils';
 
 const pifCmpPrivateConfig: PrivateConfig = {
   type: 'vendor',
