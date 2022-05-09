09add52134bd5933ba838b8924b4eda58ef7a57e
James Rosewell <james@51degrees.com>
2022-05-09 17:57:10 +0100

    Create a revised TCF Core implementation which contains only the code needed to manipulate the date and the 12 purpose flags. Test this implementation against the version which uses IAB TL TCF Core implementation to ensure the same results. This change reduces the size of the bundle and the efficiency of the project whilst using the test framework to ensure consistency with the more widely deployed IAB TCF Core project. There are no functional changes.

--- paf-mvp-cmp/src/tcfcore.ts
+++ paf-mvp-cmp/src/tcfcore.ts
@@ -1,19 +1,56 @@
-import { TCString, TCModel, SegmentEncoder, Segment } from '@iabtcf/core';
-import { Model } from './model';
-
 /**
- * Light weight module to handle the TCF Core string only wrapping the IABTechLab core library.
+ * Light weight module to handle purpose consent and dates in the TCF Core string only.
+ * See the TCF documentation for details of the bit positions in big-endian format.
  * https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/TCFv2/IAB%20Tech%20Lab%20-%20Consent%20string%20and%20vendor%20list%20formats%20v2.md?msclkid=5236f9f5c47b11ec8a04e36f3dd976c9#the-core-string
  */
+
+import { Base64Url } from './base64url';
+
 export class TcfCore {
-  // The number of purpose consent flags that are supported.
-  private static readonly purposesConsentCount = Model.MaxId;
+  // The bit index where the created date field starts.
+  private static readonly startCreated = 6;
 
-  // The current value of the TCF core model.
-  private model: TCModel;
+  // The bit index where the last updated date field starts.
+  private static readonly startLastUpdated = 42;
 
-  constructor(value: string) {
-    this.model = TCString.decode(value);
+  // The bit index where the TCF purpose consent boolean values start. Calculated from the specification.
+  private static readonly startSettings = 152;
+
+  // The number of TCF purpose consents supported by this implementation.
+  private static readonly purposes = 12;
+
+  // The total number of TCF purpose consents supported by the specification.
+  // https://github.com/InteractiveAdvertisingBureau/iabtcf-es/blob/1eeb3f92321aa241ac768d0a580f3378c70cacd8/modules/core/src/encoder/BitLength.ts#L15
+  private static readonly purposesSpec = 24;
+
+  // The number of bits needed to form a date.
+  private static readonly dateLength = 36;
+
+  // The number of bits to ignore at the start of the bit int.
+  private static dateBitIntDiff = 64 - TcfCore.dateLength;
+
+  // The maximum length of a cookie which is where the TCF string will be stored.
+  // See https://www.ietf.org/rfc/rfc2965.txt
+  private static readonly maxLength = 4096;
+
+  // The current value of the TCF core as a boolean array.
+  private buffer: boolean[];
+
+  /**
+   * Constructs a new instance of TcfCore.
+   * @param base64url encoded TCF core string
+   */
+  constructor(encodedTcfCore: string) {
+    this.buffer = Base64Url.decode(encodedTcfCore);
+    if (this.buffer.length === 0) {
+      throw 'Empty value not allowed';
+    }
+    if (this.buffer.length < TcfCore.minLength) {
+      throw `Value is '${this.buffer.length}' bytes, but must be at least '${TcfCore.minLength}' bytes`;
+    }
+    if (this.buffer.length > TcfCore.maxLength) {
+      throw `Value must be base64 string which is no longer than '${TcfCore.maxLength}' bytes`;
+    }
   }
 
   /**
@@ -25,11 +62,11 @@ export class TcfCore {
   }
 
   /**
-   * Returns the current data as a base64 string.
+   * Returns the current buffer as a base64 string.
    * @returns
    */
   toString(): string {
-    return SegmentEncoder.encode(this.model, Segment.CORE);
+    return Base64Url.encode(this.buffer);
   }
 
   /**
@@ -38,9 +75,8 @@ export class TcfCore {
    */
   getPurposesConsent(): boolean[] {
     const flags: boolean[] = [];
-    for (let bit = 0; bit < TcfCore.purposesConsentCount; bit++) {
-      // Add one to the bit to handle one based method.
-      flags.push(this.model.purposeConsents.has(bit + 1));
+    for (let bit = 0; bit < TcfCore.purposes; bit++) {
+      flags.push(this.buffer[TcfCore.startSettings + bit]);
     }
     return flags;
   }
@@ -50,33 +86,49 @@ export class TcfCore {
    * @param flags
    */
   setPurposesConsent(flags: boolean[]) {
-    if (flags.length !== TcfCore.purposesConsentCount) {
-      throw `Value must be length '${TcfCore.purposesConsentCount}' but was '${flags.length}'`;
+    if (flags.length !== TcfCore.purposes) {
+      throw `Value must be length ${TcfCore.purposes}`;
     }
-    for (let bit = 0; bit < TcfCore.purposesConsentCount; bit++) {
-      // Add one to the bit to handle one based method.
-      if (flags[bit]) {
-        this.model.purposeConsents.set(bit + 1);
-      } else {
-        this.model.purposeConsents.unset(bit + 1);
-      }
+    for (let bit = 0; bit < TcfCore.purposesSpec; bit++) {
+      const value = bit < TcfCore.purposes ? flags[bit] : false;
+      this.buffer[TcfCore.startSettings + bit] = value;
     }
   }
 
   /**
    * Sets the created and last updated date for the TCF string.
-   * @param date to set the date fields to
+   * @remarks uses the toString method to avoid complexity with bit packing
+   * @param date to set the created and updated date fields to
    */
   setDate(date: Date) {
-    this.model.created = date;
-    this.model.lastUpdated = date;
+    const value = Math.round(date.getTime() / 100);
+    const bitString = value.toString(2);
+    const falseBits = TcfCore.dateLength - bitString.length;
+    for (let i = 0; i < TcfCore.dateLength; i++) {
+      const value = i >= falseBits ? bitString[i - falseBits] === '1' : false;
+      this.buffer[i + TcfCore.startCreated] = value;
+      this.buffer[i + TcfCore.startLastUpdated] = value;
+    }
   }
 
   /**
-   * Returns the date that the TCF string was created. Used only for testing purposes.
+   * Returns the date that the TCF string was created. Used only for testing purposes and should be optimized out of the
+   * production build.
+   * @remarks uses the parseInt method to avoid complexity with bit packing
    * @param date Returns the date that the TCF string was created.
    */
   getDate(): Date {
-    return this.model.created;
+    let str = '';
+    for (let i = 0; i < TcfCore.dateLength; i++) {
+      str += this.buffer[i + TcfCore.startCreated] ? '1' : '0';
+    }
+    return new Date(parseInt(str, 2) * 100);
+  }
+
+  /**
+   * Minimum length of a TCF core string with the purpose consent flags.
+   */
+  private static get minLength(): number {
+    return Math.ceil((TcfCore.startSettings + TcfCore.purposes) / 8);
   }
 }
