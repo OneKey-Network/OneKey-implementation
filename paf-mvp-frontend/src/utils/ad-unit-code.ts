0aa697e7223c37aec286dc938f00455619ce05ab
Romain Lofaso <r.lofaso@criteo.com>
2022-05-18 12:11:44 +0200

    Glue the PrebidJs winBid event to the generation of the audit log

---
+++ paf-mvp-frontend/src/utils/ad-unit-code.ts
@@ -0,0 +1,34 @@
+import 'googletag';
+
+/**
+ * @param adUnitCode Google Publisher Tag Ad Unit Code
+ * @returns divId associated to the adUnitCode in GPT if exist, undefined otherwise.
+ */
+export function mapAdUnitCodeToDivId(adUnitCode: string): string | undefined {
+  if (!isGptPubadsDefined()) {
+    return undefined;
+  }
+
+  const slots = window.googletag.pubads().getSlots();
+  const found = slots.find((s) => {
+    return s.getAdUnitPath() === adUnitCode || s.getSlotElementId() === adUnitCode;
+  });
+
+  if (found === undefined) {
+    return undefined;
+  }
+
+  return found.getSlotElementId();
+}
+
+const isGptPubadsDefined = () => {
+  return window.googletag && isFn(window.googletag.pubads) && isFn(window.googletag.pubads().getSlots);
+};
+
+const isFn = (object: any): boolean => {
+  return isA(object, 'Function');
+};
+
+const isA = (object: any, _t: string): boolean => {
+  return toString.call(object) === `[object ${_t}]`;
+};
