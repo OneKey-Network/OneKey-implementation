823113aee14e8103a3f75f6db6fa3b29eb51f60f
Olivier Chirouze <o.chirouze@criteo.com>
2022-05-24 17:16:20 +0200

    Update example for client node

--- paf-mvp-operator-client-express/examples/example-client-node.ts
+++ paf-mvp-operator-client-express/examples/example-client-node.ts
@@ -1,39 +1,7 @@
-// FIXME update with new methods signatures
-/*
-// This is just an example of a basic client node configuration
-export const pofPublisherClientNode = new ClientNode(
-  // Identity information: mandatory for any PAF interaction
-  {
-    // Name of the PAF participant
-    name: 'Example Website',
-    // Current public key
-    publicKeys: [
-      {
-        // Timestamps are expressed in seconds
-        startTimestampInSec: getTimeStampInSec(new Date('2022-01-01T12:00:00.000Z')),
-        endTimestampInSec: getTimeStampInSec(new Date('2022-12-31T12:00:00.000Z')),
-        publicKey: `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEl0278pcupaxUfiqHJ9AG9gVMyIO+
-n07PJaNI22v+s7hR1Hkb71De6Ot5Z4JLoZ7aj1xYhFcQJsYkFlXxcBWfRQ==
------END PUBLIC KEY-----`,
-      },
-    ],
-    // Email address of DPO
-    dpoEmailAddress: 'dpo@examples-website.com',
-    // URL of a privacy page
-    privacyPolicyUrl: new URL('https://www.example-website/privacy'),
-  },
-  {
-    // The PAF node host name to receive requests
-    hostName: 'paf.example-website.com',
-    // Current private key
-    privateKey: `-----BEGIN PRIVATE KEY-----
-MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg0X8r0PYAm3mq206o
-CdMHwZ948ONyVJToeFbLqBDKi7OhRANCAASXTbvyly6lrFR+Kocn0Ab2BUzIg76f
-Ts8lo0jba/6zuFHUeRvvUN7o63lngkuhntqPXFiEVxAmxiQWVfFwFZ9F
------END PRIVATE KEY-----`,
-  },
-  // The PAF operator host
-  'example.onekey.network'
-);
-*/
+/* eslint @typescript-eslint/no-unused-vars: 0 */
+import { ClientNode } from '@operator-client/client-node';
+
+// Here is how the client is created. See config.json and public-key.pem, private-key.pem files
+(async () => {
+  const clientNode = await ClientNode.fromConfig('./config.json');
+})();
