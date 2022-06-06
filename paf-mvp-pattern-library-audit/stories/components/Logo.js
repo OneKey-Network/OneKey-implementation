e20385f22308a7d3f2b35ebe08e35b6b1778d8aa
John Norris <johntnorris@outlook.com>
2022-06-06 13:02:30 +0100

    Pattern library (Audit log v2) Add new Website Advert component

--- paf-mvp-pattern-library-audit/stories/components/Logo.js
+++ paf-mvp-pattern-library-audit/stories/components/Logo.js
@@ -1,3 +1,7 @@
 export default () => `
   <img src="images/OneKey.svg" alt="Provided by OneKey" class="ok-ui-card__header-logo" />
 `;
+
+export const LogoIcon = () => `
+  <img src="images/OneKeyIcon.svg" alt="OneKey" />
+`;
