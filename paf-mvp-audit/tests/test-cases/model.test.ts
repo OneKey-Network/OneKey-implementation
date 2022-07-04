6924b12b0d6f92038f05c62d1acebfc7554a62fe
James Rosewell <james@51degrees.com>
2022-07-04 12:22:27 +0100

    Fixed test defect where the test was not running. Now uses async await. The example data is now copied before the mock audit log is created to avoid the order of test execution impacting the test outcomes.
    (cherry picked from commit ada897d4b80bcb1aa8e2ffde4c69bf20b3343c91)

--- paf-mvp-audit/tests/test-cases/model.test.ts
+++ paf-mvp-audit/tests/test-cases/model.test.ts
@@ -15,7 +15,7 @@ describe('testing model', () => {
   beforeEach(() => {
     // Create a mock audit log ensuring that the
     mock = new AuditLogMock(log);
-    auditLog = mock.BuildAuditLog(AutomobileExample, { use_browsing_for_personalization: true });
+    auditLog = mock.BuildAuditLog({ ...AutomobileExample }, { use_browsing_for_personalization: true });
 
     // Check that the audit log appears valid.
     expect(auditLog).toBeDefined();
@@ -27,103 +27,103 @@ describe('testing model', () => {
     resolver.get(AutomobileExample.host).then((i) => expect(i.name).toBe(AutomobileExample.name));
   });
 
-  test('check identity resolution works for all nodes', () => {
-    new Model(log, resolver, auditLog).verify().then((model) => {
-      // Check the properties for all verified fields are as expected.
-      model.allVerifiedFields.forEach((i) => expect(i.value.verifiedStatus).toBe(VerifiedStatus.Valid));
-      model.allVerifiedFields.forEach((i) => expect(i.value.valid).toBe(true));
-      model.allVerifiedFields.forEach((i) => expect(i.value.identity).toBeDefined());
+  test('check identity resolution works for all nodes', async () => {
+    const model = await new Model(log, resolver, auditLog).verify();
 
-      // Check that the overall status is good.
-      expect(model.overall.value).toBe(OverallStatus.Good);
+    // Check the properties for all verified fields are as expected.
+    model.allVerifiedFields.forEach((i) => expect(i.value.verifiedStatus).toBe(VerifiedStatus.Valid));
+    model.allVerifiedFields.forEach((i) => expect(i.value.valid).toBe(true));
+    model.allVerifiedFields.forEach((i) => expect(i.value.identity).toBeDefined());
 
-      saveAuditLog('all-good.json', auditLog, resolver);
-    });
+    // Check that the overall status is good.
+    expect(model.overall.value).toBe(OverallStatus.Good);
+
+    saveAuditLog('all-good.json', auditLog, resolver);
   });
 
-  test("check identity resolution fails when one identity can't be found", () => {
+  test("check identity resolution fails when one identity can't be found", async () => {
     // Remove the 'minivan.example' domain from the list of domains that can be resolved.
     const toRemove = auditLog.transmissions.find((i) => i.source.domain === 'minivan.example');
     expect(toRemove).toBeDefined();
     resolver.map.delete(toRemove.source.domain);
 
     // Create the model.
-    new Model(log, resolver, auditLog).verify().then((model) => {
-      // Check the seed and ids and preferences fields are valid.
-      expect(model.seed.value.verifiedStatus).toBe(VerifiedStatus.Valid);
-      expect(model.idsAndPreferences.value.verifiedStatus).toBe(VerifiedStatus.Valid);
+    const model = await new Model(log, resolver, auditLog).verify();
 
-      // Find the result for the missing domain.
-      const failed = model.results.find((i) => i.value.value.source.domain === toRemove.source.domain);
-      expect(failed).toBeDefined();
+    // Check the seed and ids and preferences fields are valid.
+    expect(model.seed.value.verifiedStatus).toBe(VerifiedStatus.Valid);
+    expect(model.idsAndPreferences.value.verifiedStatus).toBe(VerifiedStatus.Valid);
 
-      // Get the verified result and check it's identity not found.
-      expect(failed.value.verifiedStatus).toBe(VerifiedStatus.IdentityNotFound);
+    // Find the result for the missing domain.
+    const failed = model.results.find((i) => i.value.value.source.domain === toRemove.source.domain);
+    expect(failed).toBeDefined();
 
-      // Check the statuses for all the other results are valid.
-      model.results.forEach((result) => {
-        if (result.value.value.source.domain !== toRemove.source.domain) {
-          expect(result.value.verifiedStatus).toBe(VerifiedStatus.Valid);
-        }
-      });
+    // Get the verified result and check it's identity not found.
+    expect(failed.value.verifiedStatus).toBe(VerifiedStatus.IdentityNotFound);
 
-      // Check the count of identity not found status is one.
-      expect(model.count(VerifiedStatus.IdentityNotFound)).toBe(1);
-
-      // Check that the overall status is caution as the identity can't be found.
-      expect(model.overall.value).toBe(OverallStatus.Suspicious);
-
-      saveAuditLog('identity-not-found.json', auditLog, resolver);
+    // Check the statuses for all the other results are valid.
+    model.results.forEach((result) => {
+      if (result.value.value.source.domain !== toRemove.source.domain) {
+        expect(result.value.verifiedStatus).toBe(VerifiedStatus.Valid);
+      }
     });
+
+    // Check the count of identity not found status is one.
+    expect(model.count(VerifiedStatus.IdentityNotFound)).toBe(1);
+
+    // Check that the overall status is caution as the identity can't be found.
+    expect(model.overall.value).toBe(OverallStatus.Suspicious);
+
+    saveAuditLog('identity-not-found.json', auditLog, resolver);
   });
 
-  test('check verification fails when one signature is corrupted', () => {
+  test('check verification fails when one signature is corrupted', async () => {
     // Corrupt the 'minivan.example' domain from the list of domains that can be resolved.
     const toCorrupt = auditLog.transmissions.find((i) => i.source.domain === 'minivan.example');
     toCorrupt.source.signature = '0' + toCorrupt.source.signature;
 
     // Create the model.
-    new Model(log, resolver, auditLog).verify().then((model) => {
-      // Check the seed and ids and preferences fields are valid.
-      expect(model.seed.value.verifiedStatus).toBe(VerifiedStatus.Valid);
-      expect(model.idsAndPreferences.value.verifiedStatus).toBe(VerifiedStatus.Valid);
+    const model = await new Model(log, resolver, auditLog).verify();
 
-      // Find the result for the missing domain.
-      const failed = model.results.find((i) => i.value.value.source.domain === toCorrupt.source.domain);
-      expect(failed).toBeDefined();
+    // Check the seed and ids and preferences fields are valid.
+    expect(model.seed.value.verifiedStatus).toBe(VerifiedStatus.Valid);
+    expect(model.idsAndPreferences.value.verifiedStatus).toBe(VerifiedStatus.Valid);
 
-      // Get the verified result and check it's identity not found.
-      expect(failed.value.verifiedStatus).toBe(VerifiedStatus.NotValid);
+    // Find the result for the missing domain.
+    const failed = model.results.find((i) => i.value.value.source.domain === toCorrupt.source.domain);
+    expect(failed).toBeDefined();
 
-      // Check the statuses for all the other results are valid.
-      model.results.forEach((result) => {
-        if (result.value.value.source.domain !== toCorrupt.source.domain) {
-          expect(result.value.verifiedStatus).toBe(VerifiedStatus.Valid);
-        }
-      });
+    // Get the verified result and check it's identity not found.
+    expect(failed.value.verifiedStatus).toBe(VerifiedStatus.NotValid);
 
-      // Check the count of not valid status is one.
-      expect(model.count(VerifiedStatus.NotValid)).toBe(1);
-
-      // Check that the overall status is bad as the signature is invalid.
-      expect(model.overall.value).toBe(OverallStatus.Violation);
-
-      saveAuditLog('corrupt-signature.json', auditLog, resolver);
+    // Check the statuses for all the other results are valid.
+    model.results.forEach((result) => {
+      if (result.value.value.source.domain !== toCorrupt.source.domain) {
+        expect(result.value.verifiedStatus).toBe(VerifiedStatus.Valid);
+      }
     });
+
+    // Check the count of not valid status is one.
+    expect(model.count(VerifiedStatus.NotValid)).toBe(1);
+
+    // Check that the overall status is bad as the signature is invalid.
+    expect(model.overall.value).toBe(OverallStatus.Violation);
+
+    saveAuditLog('corrupt-signature.json', auditLog, resolver);
   });
 
-  test('check verification when response is not success for one result', () => {
+  test('check verification when response is not success for one result', async () => {
     // Corrupt the 'minivan.example' in the list of results.
     const toCorrupt = auditLog.transmissions.find((i) => i.source.domain === 'minivan.example');
     toCorrupt.status = 'error_cannot_process';
 
     // Create the model.
-    new Model(log, resolver, auditLog).verify().then((model) => {
-      // Check that the overall status is bad as the signature is invalid.
-      expect(model.overall.value).toBe(OverallStatus.Suspicious);
+    const model = await new Model(log, resolver, auditLog).verify();
 
-      saveAuditLog('result-error.json', auditLog, resolver);
-    });
+    // Check that the overall status is bad as the signature is invalid.
+    expect(await model.overall.value).toBe(OverallStatus.Suspicious);
+
+    saveAuditLog('result-error.json', auditLog, resolver);
   });
 });
 
