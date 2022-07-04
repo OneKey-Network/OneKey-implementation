bd82cf6643fa9d55c2858eff22166b7201400e66
James Rosewell <james@51degrees.com>
2022-07-04 13:24:22 +0100

    Removed Promise from generateKey because this is only ever called in Node.

--- paf-mvp-core-js/@types/ecdsa-secp256r1/index.d.ts
+++ paf-mvp-core-js/@types/ecdsa-secp256r1/index.d.ts
@@ -3,7 +3,7 @@ declare module 'ecdsa-secp256r1' {
     verify: (string, string) => boolean | Promise<boolean>;
     sign: (string) => string;
     fromJWK: (ECKey) => IECDSA | Promise<IECDSA>;
-    generateKey: () => Promise<IECDSA>;
+    generateKey: () => IECDSA;
   }
   declare const ECDSA: IECDSA = {};
   export default ECDSA;
