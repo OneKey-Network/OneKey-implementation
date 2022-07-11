ee52629270d050d277aa006d8ac8186dae94bf47
Olivier Chirouze <o.chirouze@criteo.com>
2022-07-11 17:44:34 +0200

    Move paf-lib functions into a class (OneKeyLib)

--- paf-mvp-frontend/src/global.d.ts
+++ paf-mvp-frontend/src/global.d.ts
@@ -1,40 +1,11 @@
-import {
-  deleteIdsAndPreferences,
-  generateSeed,
-  getAuditLogByDivId,
-  getAuditLogByTransaction,
-  getIdsAndPreferences,
-  getNewId,
-  registerTransmissionResponse,
-  removeCookie,
-  setNotificationHandler,
-  setPromptHandler,
-  signPreferences,
-  updateIdsAndPreferences,
-} from './lib/paf-lib';
+import { IOneKeyLib } from './lib/paf-lib';
 import { NotificationEnum } from './enums/notification.enum';
 import { IProcessingQueue } from './utils/queue';
-import { Identifiers } from '@core/model';
-import { PafStatus } from '@frontend/enums/status.enum';
 
 export type Window = WindowProxy &
   typeof globalThis & {
-    PAF: {
+    PAF: IOneKeyLib & {
       queue?: IProcessingQueue;
-      deleteIdsAndPreferences: typeof deleteIdsAndPreferences;
-      removeCookie: typeof removeCookie;
-      getNewId: typeof getNewId;
-      signPreferences: typeof signPreferences;
-      getIdsAndPreferences: typeof getIdsAndPreferences;
-      updateIdsAndPreferences: typeof updateIdsAndPreferences;
-      setPromptHandler: typeof setPromptHandler;
-      setNotificationHandler: typeof setNotificationHandler;
-      generateSeed: typeof generateSeed;
-      registerTransmissionResponse: typeof registerTransmissionResponse;
-      getAuditLogByTransaction: typeof getAuditLogByTransaction;
-      getAuditLogByDivId: typeof getAuditLogByDivId;
-      unpersistedIds?: Identifiers;
-      status?: PafStatus;
     };
     PAFUI: {
       // FIXME remove
