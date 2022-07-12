608c560118c7a07f0a6f89e8ba3666ed5b36e544
Olivier Chirouze <o.chirouze@criteo.com>
2022-07-12 11:58:51 +0200

    Reformat paf-lib.ts to make it closer to main branch (2)

--- paf-mvp-frontend/src/lib/paf-lib.ts
+++ paf-mvp-frontend/src/lib/paf-lib.ts
@@ -34,66 +34,6 @@ import { browserName } from 'detect-browser';
 import { mapAdUnitCodeToDivId } from '@frontend/utils/ad-unit-code';
 import { buildAuditLog } from '@core/model/audit-log';
 
-// TODO ------------------------------------------------------ move to i-one-key-lib.ts START
-export enum ShowPromptOption {
-  doNotPrompt = 'doNotPrompt',
-  doPrompt = 'doPrompt',
-  promptIfUnknownUser = 'promptIfUnknownUser',
-}
-
-/**
- * Refresh result
- */
-export interface RefreshResult {
-  status: PafStatus;
-  data?: IdsAndOptionalPreferences;
-}
-
-/**
- * Refresh result
- */
-export interface IdsAndPreferencesResult {
-  status: PafStatus;
-  data?: IdsAndPreferences;
-}
-
-export type CookieData = {
-  strIds: string;
-  lastRefresh: string;
-  strPreferences: string;
-  currentlySelectedConsent: boolean;
-  currentPafData: IdsAndOptionalPreferences;
-};
-
-export interface IOneKeyLib {
-  unpersistedIds?: Identifier[];
-  triggerNotification: (initialData: CookieData, newConsent: boolean) => Promise<void>;
-  refreshIdsAndPreferences: (showPrompt?: ShowPromptOption) => Promise<RefreshResult>;
-  writeIdsAndPref: (input: IdsAndPreferences) => Promise<IdsAndOptionalPreferences | undefined>;
-  signPreferences: (input: PostSignPreferencesRequest) => Promise<Preferences>;
-  getNewId: () => Promise<Identifier>;
-  getIdsAndPreferences: () => Promise<IdsAndPreferencesResult | undefined>;
-  createSeed: (transactionIds: TransactionId[]) => Promise<SeedEntry | undefined>;
-  generateSeed: (pafTransactionIds: TransactionId[]) => Promise<Seed | undefined>;
-  registerTransmissionResponse: (
-    { prebidTransactionId, pafTransactionId, divIdOrAdUnitCode, contentId, auditHandler }: TransmissionRegistryContext,
-    transmissionResponse: TransmissionResponse
-  ) => AuditLog | undefined;
-  getAuditLogByTransaction: (prebidTransactionId: PrebidTransactionId) => AuditLog | undefined;
-  getAuditLogByDivId: (divId: DivId) => AuditLog | undefined;
-  deleteIdsAndPreferences: () => Promise<void>;
-  setPromptHandler: (handler: () => Promise<boolean>) => void;
-  promptConsent: () => Promise<boolean>;
-  setNotificationHandler: (handler: (notificationType: NotificationEnum) => Promise<void>) => void;
-  showNotification: (notificationType: NotificationEnum) => Promise<void>;
-  updateIdsAndPreferences: (
-    optIn: boolean,
-    identifiers: Identifier[]
-  ) => Promise<IdsAndOptionalPreferences | undefined>;
-  removeCookie: (cookieName: string) => void;
-}
-// TODO ------------------------------------------------------ move to i-one-key-lib.ts END
-
 // TODO ------------------------------------------------------ move to one-key-lib.ts START
 export class OneKeyLib implements IOneKeyLib {
   /**
@@ -924,3 +864,63 @@ export interface SeedEntry {
   idsAndPreferences: IdsAndPreferences;
 }
 // TODO ------------------------------------------------------ move to rtb.ts END
+
+// TODO ------------------------------------------------------ move to i-one-key-lib.ts START
+export enum ShowPromptOption {
+  doNotPrompt = 'doNotPrompt',
+  doPrompt = 'doPrompt',
+  promptIfUnknownUser = 'promptIfUnknownUser',
+}
+
+/**
+ * Refresh result
+ */
+export interface RefreshResult {
+  status: PafStatus;
+  data?: IdsAndOptionalPreferences;
+}
+
+/**
+ * Refresh result
+ */
+export interface IdsAndPreferencesResult {
+  status: PafStatus;
+  data?: IdsAndPreferences;
+}
+
+export type CookieData = {
+  strIds: string;
+  lastRefresh: string;
+  strPreferences: string;
+  currentlySelectedConsent: boolean;
+  currentPafData: IdsAndOptionalPreferences;
+};
+
+export interface IOneKeyLib {
+  unpersistedIds?: Identifier[];
+  triggerNotification: (initialData: CookieData, newConsent: boolean) => Promise<void>;
+  refreshIdsAndPreferences: (showPrompt?: ShowPromptOption) => Promise<RefreshResult>;
+  writeIdsAndPref: (input: IdsAndPreferences) => Promise<IdsAndOptionalPreferences | undefined>;
+  signPreferences: (input: PostSignPreferencesRequest) => Promise<Preferences>;
+  getNewId: () => Promise<Identifier>;
+  getIdsAndPreferences: () => Promise<IdsAndPreferencesResult | undefined>;
+  createSeed: (transactionIds: TransactionId[]) => Promise<SeedEntry | undefined>;
+  generateSeed: (pafTransactionIds: TransactionId[]) => Promise<Seed | undefined>;
+  registerTransmissionResponse: (
+    { prebidTransactionId, pafTransactionId, divIdOrAdUnitCode, contentId, auditHandler }: TransmissionRegistryContext,
+    transmissionResponse: TransmissionResponse
+  ) => AuditLog | undefined;
+  getAuditLogByTransaction: (prebidTransactionId: PrebidTransactionId) => AuditLog | undefined;
+  getAuditLogByDivId: (divId: DivId) => AuditLog | undefined;
+  deleteIdsAndPreferences: () => Promise<void>;
+  setPromptHandler: (handler: () => Promise<boolean>) => void;
+  promptConsent: () => Promise<boolean>;
+  setNotificationHandler: (handler: (notificationType: NotificationEnum) => Promise<void>) => void;
+  showNotification: (notificationType: NotificationEnum) => Promise<void>;
+  updateIdsAndPreferences: (
+    optIn: boolean,
+    identifiers: Identifier[]
+  ) => Promise<IdsAndOptionalPreferences | undefined>;
+  removeCookie: (cookieName: string) => void;
+}
+// TODO ------------------------------------------------------ move to i-one-key-lib.ts END
