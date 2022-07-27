a9b9135a6738546fad06cc46d7c45706a1abad23
Olivier Chirouze <o.chirouze@criteo.com>
2022-07-27 11:39:29 +0200

    Put back comments removed by mistake

--- paf-mvp-operator-express/examples/example-operator-api.ts
+++ paf-mvp-operator-express/examples/example-operator-api.ts
@@ -3,6 +3,7 @@ import { OperatorNode, Permission } from '@operator/operator-node';
 
 // This is just an example of a basic operator node configuration
 export const operator = new OperatorNode(
+  // Identity information: mandatory for any OneKey interaction
   {
     // Name of the OneKey participant
     name: 'Example operator',
@@ -23,12 +24,15 @@ jmcW7W3x9wvlX4YXqJUQKR2c0lveqVDj4hwO0kTZDuNRUhgxk4irwV3fzw==
     // URL of a privacy page
     privacyPolicyUrl: new URL('https://example.onekey.network/privacy'),
   },
+  // The operator host name to receive requests
   'example.onekey.network',
+  // Current private key
   `-----BEGIN PRIVATE KEY-----
 MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgxK7RQm5KP1g62SQn
 oyeE+rrDPJzpZxIyCCTHDvd1TRShRANCAAQSJkhGEbE118biXou5jZB+PJ/rRHSO
 ZxbtbfH3C+VfhheolRApHZzSW96pUOPiHA7SRNkO41FSGDGTiKvBXd/P
 -----END PRIVATE KEY-----`,
+  // List of OneKey client node host names and their corresponding permissions
   {
     'paf.example-websiteA.com': [Permission.READ, Permission.WRITE],
     'paf.example-websiteB.com': [Permission.READ, Permission.WRITE],
