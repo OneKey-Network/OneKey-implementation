fa43764f3be80a485c5a3c35efc3fc7a2557aaeb
Olivier Chirouze <o.chirouze@criteo.com>
2022-07-27 11:41:33 +0200

    Fix exports

--- paf-mvp-client-express/src/index.ts
+++ paf-mvp-client-express/src/index.ts
@@ -1,4 +1,4 @@
 export * from './client-node';
 export * from '@core/express';
 export * from '@core/errors';
-export { OriginValidator } from '@client/origin-validator';
+export * from './origin-validator';
--- paf-mvp-core-js/src/express/index.ts
+++ paf-mvp-core-js/src/express/index.ts
@@ -1,5 +1,4 @@
 export * from './utils';
 export * from './express-apps';
 export * from './config';
-export { Node } from '@core/express/node';
-export { INode } from '@core/express/node';
+export * from '@core/express/node';
