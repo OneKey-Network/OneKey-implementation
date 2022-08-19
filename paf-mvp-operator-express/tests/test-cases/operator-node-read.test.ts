50d27338e26b0aae0c04f43832a2432ef808be85
m.amineghazouani <m.amineghazouani@criteo.com>
2022-08-19 16:11:38 +0100

    Add request and permission validation middlewares for delete and getNewId endpoints

--- paf-mvp-operator-express/tests/test-cases/operator-node-read.test.ts
+++ paf-mvp-operator-express/tests/test-cases/operator-node-read.test.ts
@@ -4,6 +4,50 @@ import { NextFunction, Response } from 'express';
 import { OperatorUtils } from '../utils/operator-utils';
 import { NodeErrorType } from '@core/errors';
 
+const failCases = [
+  {
+    request: OperatorUtils.generateMockGetRequest('', false),
+    description: 'the domain is empty (postRequest)',
+    isRedirect: false,
+  },
+  {
+    request: OperatorUtils.generateMockGetRequest('unknown domain', false),
+    description: 'the domain is unknown (postRequest)',
+    isRedirect: false,
+  },
+  {
+    request: OperatorUtils.generateMockGetRequest('paf.write-only.com', false),
+    description: 'the domain does not have the read permission (postRequest)',
+    isRedirect: false,
+  },
+  {
+    request: OperatorUtils.generateMockGetRequest('', true),
+    description: 'the domain is empty (redirectRequest)',
+    isRedirect: true,
+  },
+  {
+    request: OperatorUtils.generateMockGetRequest('unknown domain', true),
+    description: 'the domain is unknown (redirectRequest)',
+    isRedirect: true,
+  },
+  {
+    request: OperatorUtils.generateMockGetRequest('paf.write-only.com', true),
+    description: 'the domain does not have the read permission (redirectRequest)',
+    isRedirect: true,
+  },
+];
+const successCases = [
+  {
+    request: OperatorUtils.generateMockGetRequest('paf.read-only.com', false),
+    description: 'the domain is authorized to read (postRequest)',
+    isRedirect: false,
+  },
+  {
+    request: OperatorUtils.generateMockGetRequest('paf.read-write.com', true),
+    description: 'the domain is authorized to read (redirectRequest)',
+    isRedirect: true,
+  },
+];
 describe('Read permission Handler', () => {
   let response: MockResponse<Response>;
   let nextFunction: NextFunction;
@@ -14,38 +58,6 @@ describe('Read permission Handler', () => {
     response = createResponse();
     nextFunction = jest.fn();
   });
-  const failCases = [
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('', false),
-      description: 'the domain is empty (postRequest)',
-      isRedirect: false,
-    },
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('unknown domain', false),
-      description: 'the domain is unknown (postRequest)',
-      isRedirect: false,
-    },
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('paf.write-only.com', false),
-      description: 'the domain does not have the read permission (postRequest)',
-      isRedirect: false,
-    },
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('', true),
-      description: 'the domain is empty (redirectRequest)',
-      isRedirect: true,
-    },
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('unknown domain', true),
-      description: 'the domain is unknown (redirectRequest)',
-      isRedirect: true,
-    },
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('paf.write-only.com', true),
-      description: 'the domain does not have the read permission (redirectRequest)',
-      isRedirect: true,
-    },
-  ];
 
   test.each(failCases)('Should pass an UNAUTHORIZED_OPERATION error to the nextFunction when $description', (input) => {
     operatorNode.buildReadPermissionHandler(input.isRedirect)(input.request, response, nextFunction);
@@ -53,21 +65,38 @@ describe('Read permission Handler', () => {
     expect(response._getStatusCode()).toEqual(400);
   });
 
-  const successCases = [
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('paf.read-only.com', false),
-      description: 'the domain is authorized to read (postRequest)',
-      isRedirect: false,
-    },
-    {
-      request: OperatorUtils.generateMockGetIdsPrefRequest('paf.read-write.com', true),
-      description: 'the domain is authorized to read (redirectRequest)',
-      isRedirect: true,
-    },
-  ];
-  test.each(successCases)('Should call the nextFunction with no error  when $description', (input) => {
+  test.each(successCases)('Should call the nextFunction with no error when $description', (input) => {
     operatorNode.buildReadPermissionHandler(input.isRedirect)(input.request, response, nextFunction);
     expect(nextFunction).toBeCalledWith();
     expect(response._getStatusCode()).toEqual(200);
   });
+});
+describe('GetNewId permission Handler', () => {
+  let response: MockResponse<Response>;
+  let nextFunction: NextFunction;
+  const operatorNode: OperatorNode = OperatorUtils.buildOperator(OperatorUtils.getUnsuccessfulJsonValidatorMock(), () =>
+    Promise.resolve({ verify: () => true })
+  );
+  beforeEach(() => {
+    response = createResponse();
+    nextFunction = jest.fn();
+  });
+
+  test.each(failCases.filter((req) => !req.isRedirect))(
+    'Should pass an UNAUTHORIZED_OPERATION error to the nextFunction when $description',
+    (input) => {
+      operatorNode.getNewIdPermissionHandler(input.request, response, nextFunction);
+      expect(nextFunction).toBeCalledWith(expect.objectContaining({ type: NodeErrorType.UNAUTHORIZED_OPERATION }));
+      expect(response._getStatusCode()).toEqual(400);
+    }
+  );
+
+  test.each(successCases.filter((req) => !req.isRedirect))(
+    'Should call the nextFunction with no error when $description',
+    (input) => {
+      operatorNode.getNewIdPermissionHandler(input.request, response, nextFunction);
+      expect(nextFunction).toBeCalledWith();
+      expect(response._getStatusCode()).toEqual(200);
+    }
+  );
 });
