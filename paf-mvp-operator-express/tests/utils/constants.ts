93ab6c9d3850c952c978132bdee5cff935104c6d
MohamedAmineGhazouani <109585792+MohamedAmineGhazouani@users.noreply.github.com>
2022-09-22 11:46:37 +0200

    add integration tests for operator write endpoint (#272)

---
+++ paf-mvp-operator-express/tests/utils/constants.ts
@@ -0,0 +1,10 @@
+import { ClientBuilder } from './client-utils';
+
+export const randomPrivateKey = `-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgiDfb74JY+vBjdEmr
+hScLNr4U4Wrp4dKKMm0Z/+h3OnahRANCAARqwDtVwGtTx+zY/5njGZxnxuGePdAq
+7fKlkuHOKtwM/AJ6oBTJ7+l3rY5ffNJZkVBB3Pt9H3cHO3Bztmh1h7xR
+-----END PRIVATE KEY-----`;
+
+export const defaultRefererUrl = `https://${ClientBuilder.defaultHost}/this/is/the/referer/page`;
+export const defaultReturnUrl = `https://${ClientBuilder.defaultHost}/this/is/the/return/url`;
