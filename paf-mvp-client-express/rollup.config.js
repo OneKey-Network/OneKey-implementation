d7d4a73df55172bbc6b16c90bfdd75c6d9dd60dd
Olivier Chirouze <o.chirouze@criteo.com>
2022-09-27 16:36:20 +0200

    Restore export of operator & client **types**.

--- paf-mvp-client-express/rollup.config.js
+++ paf-mvp-client-express/rollup.config.js
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import path, { join } from 'path';
 import { defineConfig } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
@@ -6,6 +6,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 import { builtinModules } from 'module';
+import dts from 'rollup-plugin-dts';
 import typescript from 'rollup-plugin-typescript2';
 import typescriptPaths from 'rollup-plugin-typescript-paths';
 
@@ -16,6 +17,17 @@ const entryPath = relative('src/index.ts');
 
 // https://rollupjs.org/guide/en/#configuration-files
 export default [
+  defineConfig(
+    {
+      input: {
+        index: entryPath,
+      },
+      plugins: [dts()], // Generate type declaration file index.d.ts
+      output: {
+        dir: path.dirname(pkg.types),
+        format: 'es'
+      }
+    }),
   defineConfig({
     input: entryPath,
     output: [
