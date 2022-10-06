81c42a52c292a7aa2e00ea3e972687e8c93922d5
m.amineghazouani <m.amineghazouani@criteo.com>
2022-10-06 23:45:55 +0200

    export ECDSA_NIT_P256Builder from core

--- paf-mvp-core-js/src/crypto/index.ts
+++ paf-mvp-core-js/src/crypto/index.ts
@@ -1,4 +1,5 @@
 export * from './key-store';
 export * from './verifier';
 export * from './signing-definition';
+export { IDSABuilder, ECDSA_NIT_P256Builder } from './digital-signature';
 export { isValidKey } from './identity';
