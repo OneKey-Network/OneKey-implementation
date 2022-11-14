3624491ba46580c24b8fdcf8773e3abf174f5d38
OlivierChirouze <olivier.chirouze+github@gmail.com>
2022-11-14 15:09:47 +0100

    Validate (JSON schema) "verify seed" and "verify transmission" messages (#302)

--- paf-mvp-client-express/src/client-node.ts
+++ paf-mvp-client-express/src/client-node.ts
@@ -241,6 +241,7 @@ export class ClientNode extends Node {
 
     this.setEndpointConfig('POST', jsonProxyEndpoints.verifySeed, {
       endPointName: 'VerifySeed',
+      jsonSchemaName: JsonSchemaType.verifySeedRequest,
     });
     this.app.expressApp.post(
       jsonProxyEndpoints.verifySeed,
@@ -254,6 +255,7 @@ export class ClientNode extends Node {
 
     this.setEndpointConfig('POST', jsonProxyEndpoints.verifyTransmission, {
       endPointName: 'VerifyTransmission',
+      jsonSchemaName: JsonSchemaType.verifyTransmissionRequest,
     });
     this.app.expressApp.post(
       jsonProxyEndpoints.verifyTransmission,
